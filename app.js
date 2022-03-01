359d9d315ccf5b6d1f98e5968ed38cc77974c2fd
samiisweets <samanthfeinen@gmail.com>
2022-02-28 23:00:58 -0500

    completed assignment

--- app.js
+++ app.js
@@ -4,7 +4,22 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Samantha ";
+let lastName = "Feinen";
+var age = 30;
 
 // Exercise 2
+let fullName = firstName + lastName;
+let nameFull = `${firstName}${lastName}`
+
+console.log(fullName);
+console.log(nameFull);
 
 // Exercise 3
+let city = "Manchester, NJ";
+let passtime ="crafting and playing music";
+
+let myStory = `Hello there! My name is ${firstName}${lastName} I live in ${city}. I enjoy ${passtime}!`;
+
+console.log(myStory);
+
